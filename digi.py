52ba4aba6f2aaa9c09be9583e9beba26ced486ba
Jeshan Maharjan <47115629+Jikku80@users.noreply.github.com>
2024-05-22 17:36:28 +0545

    digi.py file added which contains all the codes to train and test the model

---
+++ digi.py
@@ -0,0 +1,54 @@
+from __future__ import print_function
+import numpy as np
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.optimizers import Adam
+from tensorflow.keras.utils import to_categorical
+import tensorflow as tf
+
+np.random.seed(1671)
+
+NB_EPOCH = 250
+BATCH_SIZE = 128
+VERBOSE = 1
+NB_CLASSES = 10
+OPTIMIZER = Adam()
+N_HIDDEN = 128
+VALIDATION_SPLIT = 0.2
+DROPOUT = 0.3
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+RESHAPED = 784
+
+X_train = X_train.reshape(60000, RESHAPED)
+X_test = X_test.reshape(10000, RESHAPED)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+X_train /= 255
+X_test /= 255
+
+Y_train = to_categorical(y_train, NB_CLASSES)
+Y_test = to_categorical(y_test, NB_CLASSES)
+
+model = Sequential()
+model.add(Dense(N_HIDDEN, input_shape=(RESHAPED,)))
+model.add(Activation('relu'))
+model.add(Dropout(DROPOUT))
+model.add(Dense(N_HIDDEN))
+model.add(Activation('relu'))
+model.add(Dropout(DROPOUT))
+model.add(Dense(NB_CLASSES))
+model.add(Activation('softmax'))
+model.summary()
+
+model.compile(loss='categorical_crossentropy', optimizer = OPTIMIZER, metrics=['accuracy'])
+#training the model
+history = model.fit(X_train, Y_train, batch_size = BATCH_SIZE, epochs = NB_EPOCH, verbose = VERBOSE, validation_split = VALIDATION_SPLIT)
+#Save model
+tf.saved_model.save(model, 'C:/Users/jamsm/Downloads')
+#testing the score and accuracy
+score = model.evaluate(X_test, Y_test, verbose = VERBOSE)
+print("Test Score: ", score[0])
+print("Test Accuracy: ", score[1])
